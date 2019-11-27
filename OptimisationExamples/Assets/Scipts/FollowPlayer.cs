7d6b5588684507c8907bedfa20021669b7a92fa1
Jan Julin <mummo94@hotmail.com>
2019-11-27 21:59:43 +0200

    Basic third person controller added to player

---
+++ OptimisationExamples/Assets/Scipts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform playerToFollow;
+    private Vector3 offset;
+
+
+    private void Awake()
+    {
+        offset = gameObject.transform.position;
+    }
+    private void LateUpdate()
+    {
+        gameObject.transform.position = playerToFollow.transform.position + offset;
+    }
+}
--- OptimisationExamples/Assets/Scipts/PlayerController.cs
+++ OptimisationExamples/Assets/Scipts/PlayerController.cs
@@ -4,15 +4,79 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float movementSpeed = 10;
+
+    public float jumpSpeed = 10;
+
+    private Vector3 movementDirection = Vector3.zero;
+    private Rigidbody myRB;
+
+
+    private void Awake()
     {
-        
+        myRB = gameObject.GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        movementDirection = Vector3.zero;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            movementDirection -= gameObject.transform.right;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movementDirection += gameObject.transform.right;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            movementDirection += gameObject.transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movementDirection -= gameObject.transform.forward;
+        }
+        Turning();
+        Move();
+    }
+
+    private void Turning()
+    {
+        Vector3 mouseScreenPosition = Input.mousePosition;
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3 (mouseScreenPosition.x, mouseScreenPosition.y, Camera.main.transform.position.y));
+        mouseWorldPosition.y = gameObject.transform.position.y;
+        gameObject.transform.LookAt(mouseWorldPosition);
+    }
+
+    #region Turning2.0
+
+    //Camera.main call is basically GameObject.FindObjectWithTag("MainCamera"); which is unnecessary heavy call
+    private Camera mainCamera;
+    //Reducing garbage by making variables just once
+    private Vector3 mouseScreenPosition2;
+    private Vector3 mouseWorldPosition2;
+    private void Turning2()
+    {
+        mouseScreenPosition2= Input.mousePosition;
+        mouseWorldPosition2 = mainCamera.ScreenToWorldPoint(new Vector3(mouseScreenPosition2.x, mouseScreenPosition2.y, mainCamera.transform.position.y));
+        mouseWorldPosition2.y = gameObject.transform.position.y;
+        gameObject.transform.LookAt(mouseWorldPosition2);
+    }
+
+    #endregion
+
+    private void Move()
+    {
+        gameObject.transform.localPosition += new Vector3(movementDirection.x, 0, movementDirection.z) * movementSpeed * Time.deltaTime;
+    }
+
+    private void Jump()
+    {
+        myRB.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
     }
 }
